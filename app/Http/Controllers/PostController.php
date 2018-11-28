ff799d3c3c16b361fe88b176acdc4071fa340a76
Max Solomon <max.solomon@bbtravel.local>
2018-11-28 12:22:47 +0000

    More changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,6 +70,7 @@ class PostController extends Controller
                                   'author_name' => Auth::user()->name,
                                   'author_id' => Auth::user()->id, ]);
 
+
         return redirect('post/'.$post->id);
     }
 
