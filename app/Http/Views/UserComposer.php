fe47ab55d43885604d93a04a80a9d6b6e3389f37
maxsolomon14 <max.solomon@bluebaytravel.co.uk>
2018-11-28 16:05:10 +0000

    Apply fixes from StyleCI

--- app/Http/Views/UserComposer.php
+++ app/Http/Views/UserComposer.php
@@ -10,7 +10,8 @@ class UserComposer
     /**
      * Bind data to the view.
      *
-     * @param  View  $view
+     * @param View $view
+     *
      * @return void
      */
     public function compose(View $view)
