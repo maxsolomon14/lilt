35f8c1c910c3a1addac61d0637100076dc52d41f
maxsolomon14 <max.solomon@bluebaytravel.co.uk>
2018-11-28 16:24:32 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -57,21 +57,20 @@ class PagesController extends Controller
             $criteria = $request->search;
 
             if ($request->search_type === 'users') {
-                $user = User::where('name', 'like', '%' . $criteria . '%')->first();
+                $user = User::where('name', 'like', '%'.$criteria.'%')->first();
                 if ($user == null) {
                     return redirect(url()->previous());
                 } else {
-                    return redirect('/profile/' . $user->id);
+                    return redirect('/profile/'.$user->id);
                 }
             }
 
             if ($request->search_type === 'post') {
-                $post = Post::where('title', 'like', '%' . $criteria . '%')->first();
+                $post = Post::where('title', 'like', '%'.$criteria.'%')->first();
                 if ($post == null) {
                     return redirect(url()->previous());
-
                 } else {
-                    return redirect('/post/' . $post->id);
+                    return redirect('/post/'.$post->id);
                 }
             }
 
